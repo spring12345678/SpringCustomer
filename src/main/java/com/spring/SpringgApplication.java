3ec5352bf2d3be57ac534da137e3918c1b0ff302
Twinkle1111 <40847612+Twinkle1111@users.noreply.github.com>
2018-07-29 00:30:03 +0530

    Add files via upload

---
+++ src/main/java/com/spring/SpringgApplication.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgApplication.class, args);
+	}
+
+}
